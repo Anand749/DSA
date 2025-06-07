fb61bf1050cd42fdf5e494194e310b1425097632
Anand749 <anandchapke11@gmail.com>
2025-06-07 11:36:52 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (48.61%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -6,43 +6,32 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if (!root) return {};
         vector<vector<int>> result;
-        if (root == NULL)
-            return result;
-
         queue<TreeNode*> q;
         q.push(root);
-        bool l_t_r = true;
+        bool leftToRight = true;
 
         while (!q.empty()) {
-            int size = q.size();
-            vector<int> row(size);
-            for (int i = 0; i < size; i++) {
+            int levelSize = q.size();
+            vector<int> level(levelSize);
+            for (int i = 0; i < levelSize; ++i) {
                 TreeNode* node = q.front();
                 q.pop();
+                int index = leftToRight ? i : (levelSize - 1 - i);
+                level[index] = node->val;
 
-                int index = (l_t_r) ? i : (size - 1 - i);
-
-                row[index] = node->val;
-            if (node->left){
- q.push(node->left);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
             }
-                    
-                
-                 if (node->right){
-                    q.push(node->right);
-                 }
-                  
-            }
-            l_t_r = !l_t_r;
-            result.push_back(row);
+            leftToRight = !leftToRight;
+            result.push_back(level);
         }
 
         return result;
