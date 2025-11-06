43d23ddfec61adb6fbaa04b615ee9f3e2fe244b8
Anand749 <anandchapke11@gmail.com>
2025-11-06 12:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (21.99%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        int n = s.size();
-        int i = 0;
-        long long ans = 0;
-        int sign = 1;
+    int myAtoiRec(string& s, int i, int n, long long curr, int sign) {
 
+        if (i >= n || !isdigit(s[i]))
+            return curr * sign;
+
+        curr = curr * 10 + (s[i] - '0');
+
+        if (curr * sign >= INT_MAX)return INT_MAX;
+        if (curr * sign <= INT_MIN)return INT_MIN;
+
+        
+        return myAtoiRec(s, i + 1, n, curr, sign);
+    }
+
+    int myAtoi(string s) {
+        int i = 0, n = s.size(), sign = 1;
         while (i < n && s[i] == ' ')
             i++;
 
-         if (i < n && s[i] == '-') {
-            sign = -1;
-            i++;
-        } 
-        else if (i < n && s[i] == '+') {
-            sign = 1;
-            i++;
-        };
+        if (i < n && (s[i] == '-' || s[i] == '+')) {
+            sign = (s[i] == '-') ? -1 : 1;
 
-        while (i < n && isdigit(s[i])) {
-            int diff = s[i] - '0';
-            ans = ans * 10 + diff;
-            if (ans * sign > INT_MAX) return INT_MAX;
-            if (ans * sign < INT_MIN) return INT_MIN;
             i++;
         }
-
-        return ans * sign;
+        return myAtoiRec(s, i, n, 0, sign);
     }
 };
