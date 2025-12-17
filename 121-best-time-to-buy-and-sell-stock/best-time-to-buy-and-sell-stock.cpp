6eab2e39fe14df43287b83590643dc43c41ad585
Anand <anandchapke11@gmail.com>
2025-12-17 09:46:37 +0530

    Time: 12 ms (7.01%) | Memory: 97.3 MB (60.05%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,41 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& nums) {
+//         int maxi = INT_MIN;
+//         int sum = 0;
+
+//         if(nums.size()==1)return 0;
+
+//         for (int i = 1; i < nums.size(); i++) {
+//             int diff = nums[i] - nums[i - 1];
+//             sum += diff;
+//             if (sum > maxi)
+//                 maxi = diff;
+
+//             if (sum < 0)
+//                 sum = 0;
+
+//             maxi = max(maxi, sum);
+//         }
+
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& nums) {
-        int maxi = INT_MIN;
-        int sum = 0;
-        if(nums.size()==1)return 0;
+        int maxProfit = 0;
+        int minProfit = nums[0];
+
         for (int i = 1; i < nums.size(); i++) {
-            int diff = nums[i] - nums[i - 1];
-            sum += diff;
-            if (sum > maxi)
-                maxi = diff;
 
-            if (sum < 0)
-                sum = 0;
+            int diff = nums[i] - minProfit;
 
-            maxi = max(maxi, sum);
+            minProfit = min(minProfit, nums[i]);
+            maxProfit = max(maxProfit, diff);
         }
 
-        return maxi;
+        return maxProfit;
     }
 };
