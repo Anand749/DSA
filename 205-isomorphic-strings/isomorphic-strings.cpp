c3e347a8f689764b895841fae0dae61e86a88220
Anand <anandchapke11@gmail.com>
2025-12-20 10:19:45 +0530

    Time: 4 ms (22.43%) | Memory: 9.3 MB (78.57%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.size() != t.size()) 
-        {
+        int a1 = s.size();
+        int t1 = t.size();
+
+        if (a1 != t1)
             return false;
-        }
 
         unordered_map<char, char> app, ap;
+
         for (int i = 0; i < s.size(); i++) {
-            if (app.count(s[i]) && app[s[i]] != t[i]) return false;
-            if (ap.count(t[i]) && ap[t[i]] != s[i]) return false;
-            app[s[i]] = t[i];
-            ap[t[i]] = s[i];
+
+           if(app.count(s[i])&& app[s[i]]!=t[i])return false;
+           if(ap.count(t[i])&& ap[t[i]]!=s[i])return false;
+
+           app[s[i]]=t[i];
+           ap[t[i]]=s[i];
         }
+
         return true;
     }
 };
